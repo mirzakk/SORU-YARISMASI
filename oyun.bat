6489b76339fce6b5ae26795305ea1f4127aefd8d
MirzAbi <100478671+mirzakk@users.noreply.github.com>
2022-09-04 20:42:04 +0300

    Add files via upload

---
+++ oyun.bat
@@ -0,0 +1,933 @@
+@echo off
+
+@color 2f
+
+msg * Bu Oyun MIRZA KIRAC Tarafindan Uretilmistir !!!
+
+PAUSE
+
+msg * Bilgi Yarismasidir
+
+pause
+
+:m
+
+cls
+
+echo Dunyanin En fazla nufusuna sahip ulke Neresidir ??
+
+echo 1.Turkiye
+
+echo 2.Amerika
+
+echo 3.Rusya
+
+echo 4.Cin 
+
+set input=nothing
+
+set/p input=Sec
+
+if %input%==1 goto bir
+
+if %input%==2 goto iki
+
+if %input%==3 goto üç
+
+if %input%==4 goto dört
+
+goto m
+
+:bir
+
+msg * Yanlis Cevap
+
+pause
+
+msg * Oyun Bitti Tekrar Dene
+
+pause
+
+goto m
+
+:iki
+
+msg * Yanlis Cevap
+
+pause
+
+msg * Oyun Bitti Tekrar Dene
+
+pause
+
+cls
+
+goto m
+
+:üç
+
+msg * Yanlis Cevap
+
+pause
+
+msg * Oyun Bitti Tekrar Dene
+
+pause
+
+cls
+
+goto m
+
+:dört
+
+msg * Tebrikler Dogru Cevap
+
+msg * Diger Soruya Gecelim
+
+pause
+
+cls
+
+echo Turkiyede Cumhuriyet Kac Yilinda ilan Edildi
+
+echo 1.1920
+
+echo 2.1923
+
+echo 3.1932
+
+echo 4.1910
+
+set input=nothing
+
+set/p input=Sec
+
+if %input%==1 goto bir
+
+if %input%==2 goto iki
+
+if %input%==3 goto üç
+
+if %input%==4 goto dört
+
+goto m
+
+:dört
+
+msg * Yanlis Cevap
+
+msg * Oyun Bitti Tekrar Dene
+
+pause
+
+goto m
+
+:üç
+
+msg * Yanlis Cevap
+
+msg * Oyun Bitti Tekrar Dene
+
+pause
+
+goto m
+
+:bir
+
+msg * Yanlis Cevap
+
+msg * Oyun Bitti Tekrar Dene
+
+pause
+
+goto m
+
+:iki
+
+msg * Dogru Cevap
+
+msg * Diger Soruya Gecelim
+
+pause
+
+echo Dunyada En Fazla Kullanilan Teknolojik Alet Hangisidir ??
+
+echo 1.Bilgisayar
+
+echo 2.Televizyon
+
+echo 3.Telefon
+
+echo 4.Radyo
+
+set input=nothin
+
+set/p input=Sec
+
+if %input%==1 goto bir
+
+if %input%==2 goto iki
+
+if %input%==3 goto üç
+
+if %input%==4 goto dört
+
+goto m
+
+:bir
+
+msg * Yanlis Cevap
+
+msg * Oyun Bitti Tekrar Dene
+
+pause
+
+goto m
+
+:iki
+
+msg * Yanlis Cevap
+
+msg * Oyun Bitti Tekrar Dene
+
+pause
+
+goto m
+
+:dört
+
+msg * Yanlis Cevap
+
+msg * Oyun Bitti Tekrar Dene
+
+pause
+
+goto m
+
+:üç
+
+msg * Dogru Cevap
+
+msg * Diger Soruya Gecelim
+
+pause
+
+echo Bazi Aylar 30, Bazilari 31 Ceker; Kac Ayda 28 Gun Vardir ??
+
+echo 1.Ocakta
+
+echo 2.Subatta
+
+echo 3.Hepsinde
+
+echo 4.Kasimda
+
+set input=nothing
+
+set/p input=Sec
+
+if %input%==1 goto bir
+
+if %input%==2 goto iki
+
+if %input%==3 goto üç
+
+if %input%==4 goto dört
+
+goto m
+
+:bir
+
+echo Yanlis Cevap
+
+msg * Oyun Bitti Tekrar Dene
+
+pause
+
+goto m
+
+:iki
+
+echo Yanlis Cevap
+
+msg * Oyun Bitti Tekrar Dene
+
+pause
+
+goto m
+
+:dört
+
+msg * Yanlis Cevap
+
+msg * Oyun Bitti Tekrar Dene
+
+pause
+
+goto m
+
+:üç
+
+msg * Her Ayda 28 Gun Vardir 
+
+msg * Tebrikler Dogru Cevap
+
+msg * Diger Soruya Gecelim
+
+pause
+
+echo Insanin En Buyuk Hazinesi Hangisidir ??
+
+echo 1.Ailesi
+
+echo 2.Parasi
+
+echo 3.Mali Mulku
+
+echo 4.Evi
+
+set input=nothing
+
+set/p input=Sec
+
+if %input%==1 goto bir
+
+if %input%==2 goto iki
+
+if %input%==3 goto üç
+
+if %input%==4 goto dört
+
+goto m
+
+:iki
+
+echo Yanlis Cevap
+
+msg * Oyun Bitti Tekrar Dene
+
+pause
+
+goto m
+
+:üç
+
+echo Yanlis Cevap
+
+msg * Oyun Bitti Tekrar Dene
+
+pause
+
+goto m
+
+:dört
+
+echo Yanlis Cevap
+
+msg * Oyun Bitti Tekrar Dene
+
+goto m
+
+pause
+
+:bir
+
+msg * Tebrikler Dogru Cevap
+
+msg * Diger Soruya Gecelim
+
+pause
+
+echo Annenin ve Babanin Cocugu Olup Kardesin Olmayan Kisi Kimdir ??
+
+echo 1.Kendin
+
+echo 2.Baban
+
+echo 3.Deden
+
+echo 4.Anneannen
+
+set input=nothing
+
+set/p input=Sec
+
+if %input%==1 goto bir
+
+if %input%==2 goto iki
+
+if %input%==3 goto üç
+
+if %input%==4 goto dört
+
+goto m
+
+:iki
+
+echo Yanlis Cevap
+
+msg * Oyun Bitti Tekrar Dene
+
+pause
+
+goto m
+
+:üç
+
+echo Yanlis Cevap
+
+msg * Oyun Bitti Tekrar Dene
+
+pause
+
+goto m
+
+:dört
+
+echo Yanlis Cevap
+
+msg * Oyun Bitti Tekrar Dene
+
+goto m
+
+pause
+
+:bir
+
+msg * Tebrikler Dogru Cevap
+
+msg * Diger Soruya Gecelim
+
+pause
+
+echo Parcalanamaz Bir Duvara Durdurulamaz Bir Tren Carparsa Ne Olur ??
+
+echo 1.Duvar Yikilir 
+
+echo 2.Paradox
+
+echo 3.Tren Parcalanir
+
+echo 4.Ikiside Parcalanir
+
+set input=nothing
+
+set/p input=Sec
+
+if %input%==1 goto bir
+
+if %input%==2 goto iki
+
+if %input%==3 goto üç
+
+if %input%==4 goto dört
+
+goto m
+
+:bir
+
+echo Yanlis Cevap
+
+msg * Oyun Bitti Tekrar Dene
+
+pause
+
+goto m
+
+:üç
+
+echo Yanlis Cevap
+
+msg * Oyun Bitti Tekrar Dene
+
+pause
+
+goto m
+
+:dört
+
+echo Yanlis Cevap
+
+msg * Oyun Bitti Tekrar Dene
+
+goto m
+
+pause
+
+:iki
+
+msg * Tebrikler Dogru Cevap
+
+msg * Diger Soruya Gecelim
+
+pause
+
+echo Doktorunuz Size 3 Hap Verir ve Bunlari Yarimsar Saat Arayla Almanizi Tavsiye Ederse, Ilaclarin Tamamini Bitirmeniz ne Kadar Surer ??
+
+echo 1.Yarim Saat 
+
+echo 2.Bir Saat
+
+echo 3.Bir Bucuk Saat
+
+echo 4.Iki Saat
+
+set input=nothing
+
+set/p input=Sec
+
+if %input%==1 goto bir
+
+if %input%==2 goto iki
+
+if %input%==3 goto üç
+
+if %input%==4 goto dört
+
+goto m
+
+:bir
+
+echo Yanlis Cevap
+
+msg * Oyun Bitti Tekrar Dene
+
+pause
+
+goto m
+
+:üç
+
+echo Yanlis Cevap
+
+msg * Oyun Bitti Tekrar Dene
+
+pause
+
+goto m
+
+:dört
+
+echo Yanlis Cevap
+
+msg * Oyun Bitti Tekrar Dene
+
+goto m
+
+pause
+
+:iki
+
+msg * Tebrikler Dogru Cevap
+
+msg * Diger Soruya Gecelim
+
+pause
+
+echo Bir Ciftcinin 17 Koyunu Vardi. Surude Salgin Hastalik oldu, Dokuzu Agir Hastalandi, Digerleri Oldu. Ciftcinin Kac Koyunu Var ??
+
+echo 1.0 
+
+echo 2.9
+
+echo 3.6
+
+echo 4.17
+
+set input=nothing
+
+set/p input=Sec
+
+if %input%==1 goto bir
+
+if %input%==2 goto iki
+
+if %input%==3 goto üç
+
+if %input%==4 goto dört
+
+goto m
+
+:bir
+
+echo Yanlis Cevap
+
+msg * Oyun Bitti Tekrar Dene
+
+pause
+
+goto m
+
+:üç
+
+echo Yanlis Cevap
+
+msg * Oyun Bitti Tekrar Dene
+
+pause
+
+goto m
+
+:dört
+
+echo Yanlis Cevap
+
+msg * Oyun Bitti Tekrar Dene
+
+goto m
+
+pause
+
+:iki
+
+msg * Tebrikler Dogru Cevap
+
+msg * Diger Soruya Gecelim
+
+pause
+
+echo Gece Saat Sekizde Yatiyorum ve Yatarken Guguklu Saatimi Sabah dokuza kuruyorum kac saat uyurum ??
+
+echo 1.13
+
+echo 2.12
+
+echo 3.1
+
+echo 4.8
+
+set input=nothing
+
+set/p input=Sec
+
+if %input%==1 goto bir
+
+if %input%==2 goto iki
+
+if %input%==3 goto üç
+
+if %input%==4 goto dört
+
+goto m
+
+:bir
+
+echo Yanlis Cevap
+
+msg * Oyun Bitti Tekrar Dene
+
+msg * Guguklu Saatler Gece Gunduz ayrimi Yapamadigi Icin Sadece 1 Saat Uyursunuz
+
+pause
+
+goto m
+
+:iki
+
+echo Yanlis Cevap
+
+msg * Oyun Bitti Tekrar Dene
+
+msg * Guguklu Saatler Gece Gunduz ayrimi Yapamadigi Icin Sadece 1 Saat Uyursunuz
+
+pause
+
+goto m
+
+:dört
+
+echo Yanlis Cevap
+
+msg * Oyun Bitti Tekrar Dene
+
+msg * Guguklu Saatler Gece Gunduz ayrimi Yapamadigi Icin Sadece 1 Saat Uyursunuz
+
+goto m
+
+pause
+
+:üç
+
+msg * Tebrikler Dogru Cevap
+
+msg * Diger Soruya Gecelim
+
+msg * Guguklu Saatler Gece Gunduz ayrimi Yapamadigi Icin Sadece 1 Saat Uyursunuz
+
+pause
+
+echo 30 u Yarima Bolup 10 Eklediniz, Kac Etti ??
+
+echo 1.10
+
+echo 2.25
+
+echo 3.70
+
+echo 4.35
+
+set input=nothing
+
+set/p input=Sec
+
+if %input%==1 goto bir
+
+if %input%==2 goto iki
+
+if %input%==3 goto üç
+
+if %input%==4 goto dört
+
+goto m
+
+:bir
+
+echo Yanlis Cevap
+
+msg * Oyun Bitti Tekrar Dene
+
+msg * 70 eder. Yarima (1/2) Bolmek, 2 ile Carpmak Demektir
+
+pause
+
+goto m
+
+:iki
+
+echo Yanlis Cevap
+
+msg * Oyun Bitti Tekrar Dene
+
+msg * 70 eder. Yarima (1/2) Bolmek, 2 ile Carpmak Demektir
+
+pause
+
+goto m
+
+:dört
+
+echo Yanlis Cevap
+
+msg * Oyun Bitti Tekrar Dene
+
+msg * 70 eder. Yarima (1/2) Bolmek, 2 ile Carpmak Demektir
+
+goto m
+
+pause
+
+:üç
+
+msg * Tebrikler Dogru Cevap
+
+msg * Diger Soruya Gecelim
+
+msg * 70 eder. Yarima (1/2) Bolmek, 2 ile Carpmak Demektir
+
+pause
+
+echo Sadece bir tek kibritiniz var, icinde bir gaz lambasi, bir gaz sobasi, ve bir de mum bulunan karanlik ve soguk bir odaya girdiniz… Once Hangisini yakarsiniz ??
+
+echo 1.Gaz Lambasini
+
+echo 2.Gaz Sobasini
+
+echo 3.Mumu
+
+echo 4.Kibriti
+
+set input=nothing
+
+set/p input=Sec
+
+if %input%==1 goto bir
+
+if %input%==2 goto iki
+
+if %input%==3 goto üç
+
+if %input%==4 goto dört
+
+goto m
+
+:bir
+
+echo Yanlis Cevap
+
+msg * Oyun Bitti Tekrar Dene
+
+pause
+
+goto m
+
+:iki
+
+echo Yanlis Cevap
+
+msg * Oyun Bitti Tekrar Dene
+
+pause
+
+goto m
+
+:üç
+
+echo Yanlis Cevap
+
+msg * Oyun Bitti Tekrar Dene
+
+goto m
+
+pause
+
+:dört
+
+msg * Tebrikler Dogru Cevap
+
+msg * Diger Soruya Gecelim
+
+pause
+
+echo Hz. Musa Gemisine Her Hayvandan Kacar Adet Aldi ??
+
+echo 1.3
+
+echo 2.0
+
+echo 3.1
+
+echo 4.2
+
+set input=nothing
+
+set/p input=Sec
+
+if %input%==1 goto bir
+
+if %input%==2 goto iki
+
+if %input%==3 goto üç
+
+if %input%==4 goto dört
+
+goto m
+
+:bir
+
+echo Yanlis Cevap
+
+msg * Oyun Bitti Tekrar Dene
+
+msg * 0 Adet Cunku Gemisine Hayvan Alan Nuh Peygamber idi
+
+pause
+
+goto m
+
+:üç
+
+echo Yanlis Cevap
+
+msg * Oyun Bitti Tekrar Dene
+
+msg * 0 Adet Cunku Gemisine Hayvan Alan Nuh Peygamber idi
+
+pause
+
+goto m
+
+:dört
+
+echo Yanlis Cevap
+
+msg * Oyun Bitti Tekrar Dene
+
+msg * 0 Adet Cunku Gemisine Hayvan Alan Nuh Peygamber idi
+
+goto m
+
+pause
+
+:iki
+
+msg * Tebrikler Dogru Cevap
+
+msg * Diger Soruya Gecelim
+
+msg * 0 Adet Cunku Gemisine Hayvan Alan Nuh Peygamber idi
+
+pause
+
+echo 3 Elmam Vardi, Ikisini aldin. Kac Elman Var ??
+
+echo 1.0
+
+echo 2.1
+
+echo 3.2
+
+echo 4.3
+
+set input=nothing
+
+set/p input=Sec
+
+if %input%==1 goto bir
+
+if %input%==2 goto iki
+
+if %input%==3 goto üç
+
+if %input%==4 goto dört
+
+goto m
+
+:bir
+
+echo Yanlis Cevap
+
+msg * Oyun Bitti Tekrar Dene
+
+pause
+
+goto m
+
+:iki
+
+echo Yanlis Cevap
+
+msg * Oyun Bitti Tekrar Dene
+
+pause
+
+goto m
+
+:dört
+
+echo Yanlis Cevap
+
+msg * Oyun Bitti Tekrar Dene
+
+goto m
+
+pause
+
+:üç
+
+msg * Tebrikler Dogru Cevap
+
+msg * Oyunumuz Burda Bitmistir
+
+msg * 4 te Kac Yaptiniz Yorumlara Yazin
+
+msg * Devami Gelecektir
+
+msg * Youtube Kanalimiza abone olursaniz beni desteklemis olursunuz
+
+start https://www.youtube.com/channel/UCv0kT_61Mwy3iJIRej9zxlQ?view_as=subscriber
+
+pause
+
+ 
+
+
